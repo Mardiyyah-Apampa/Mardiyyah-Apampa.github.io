d89c275c35a4256d20e9963e8c45e00643fe148a
Mardiyyah <apampamardiyyah@gmail.com>
2025-05-07 14:32:22 +0100

    Update content

---
+++ build/precache-manifest.829d8398a358fc323e565462c263c5b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f13a218edd2acd43e964e2a09a9fb24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc838fbeeb7beaecddd5",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ef32ff13c3c5d708ba5d",
+    "url": "/static/css/main.fe44ea1e.chunk.css"
+  },
+  {
+    "revision": "bc838fbeeb7beaecddd5",
+    "url": "/static/js/2.210ade3f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.210ade3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef32ff13c3c5d708ba5d",
+    "url": "/static/js/main.fd06b3bd.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
